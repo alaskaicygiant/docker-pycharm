fbe69addfe0a9553a88c39ace0dbee9c37e2cf36
Your Name <you@example.com>
2016-05-04 07:23:32 +0000

    remove unused settings

--- .PyCharm2016.1/system/tmp/git-ssh-1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/usr/lib/pycharm/jre/jre/bin/java" -cp "/usr/lib/pycharm/plugins/git4idea/lib/git4idea-rt.jar:/usr/lib/pycharm/lib/xmlrpc-2.0.jar:/usr/lib/pycharm/lib/commons-codec-1.9.jar:/usr/lib/pycharm/lib/util.jar:/usr/lib/pycharm/plugins/git4idea/lib/trilead-ssh2.jar" org.jetbrains.git4idea.ssh.SSHMain "$@"
